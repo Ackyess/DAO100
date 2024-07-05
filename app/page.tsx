51654056a5b08eb821dbfb8704d7bcc183cad788
Ackyess <133567825+Ackyess@users.noreply.github.com>
2024-07-05 21:13:03 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,39 @@ export default function Home() {
         [array[i], array[j]] = [array[j], array[i]];
       }
     };
-    const cardData = [];
+    const cardData = [
+      {
+        title: "走向天空的人",
+        url: "https://neu11.learnwithgpt.space/",
+        description: "Access to GPT-3.5",
+      },
+      {
+        title: "轨道反射器",
+        url: "https://neu12.learnwithgpt.space/",
+        description: " Access to GPT-3.5",
+      },
+      {
+        title: "洞穴之喻",
+        url: "https://neu13.learnwithgpt.space/",
+        description: " Access to GPT-3.5",
+      },
+      {
+        title: "时钟总是错的",
+        url: "https://neu14.learnwithgpt.space/",
+        description: " Access to GPT-3.5",
+      },
+      {
+        title: "二分之一个月亮",
+        url: "https://neu15.learnwithgpt.space/",
+        description: " Access to GPT-3.5",
+      },
+      {
+        title: "关于无的无所有",
+        url: "https://neu16.learnwithgpt.space/",
+        description: " Access to GPT-3.5",
+      },
+
+    ];
 
     shuffleArray(cardData);
     setCards(cardData);
@@ -27,7 +59,7 @@ export default function Home() {
           <code className={styles.code}>ChatGPT</code>
         </p>
         <div></div>
-        <a style={{ color: "#808080" }}>Supported by DAO </a>
+        <a style={{ color: "#808080" }}>Supported by DAO  </a>
       </div>
 
       <div className={styles.center}>
@@ -40,11 +72,11 @@ export default function Home() {
           priority
         />
       </div>
-      <div className={styles.center}>
-        <p className={styles.tip}>
-          网站因负载过高, 临时迁移。有问题联系我, qq:34270764(备注来意{" "}
-        </p>
-      </div>
+       <div className={styles.center}>
+<p className={styles.tip}>
+  网站因负载过高, 临时迁移。有问题联系我, qq:34270764(备注来意{" "}
+  </p>
+  </div>
       <div className={styles.grid}>
         {cards.map((card, index) => (
           <a key={index} href={card.url} className={styles.card}>
@@ -56,9 +88,12 @@ export default function Home() {
 
       <div>
         <p className={styles.tip}>
-          <p className={styles.spacer}></p>
+             
+          tips：每次页面刷新会打乱排序，请记住之前使用的账号名称。如果某个账号无法使用，可以换一个用。显示未找到share_token，需更换国内网络。对话内容可能会受到审查，请不要问不文明的问题
+           <p className={styles.spacer}></p>
         </p>
       </div>
+      
     </main>
   );
 }
